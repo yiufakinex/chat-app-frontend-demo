67bea3123b61e036ba4250d96848523c008cdc16
Fa Kin Yiu, Franklin <lifeaskrystal@hotmail.com>
2024-06-02 12:14:12 -0600

    Added AuthenticationProvider Type

--- src/types/User.tsx
+++ src/types/User.tsx
@@ -3,6 +3,7 @@ export type User = {
     username: string,
     displayName: string,
     avatarURL?: string,
+    authenticationProvider: AuthenticationProvider,
     role: Role,
     createdAt: number
 }
@@ -10,3 +11,5 @@ export type User = {
 export const DefaultPicture: string = 'https://lh3.googleusercontent.com/-cXXaVVq8nMM/AAAAAAAAAAI/AAAAAAAAAKI/_Y1WfBiSnRI/photo.jpg';
 
 type Role = 'NEW_USER' | 'USER' | 'ADMIN' | 'BANNED';
+
+type AuthenticationProvider = 'GITHUB' | 'GOOGLE' | 'DISCORD';
